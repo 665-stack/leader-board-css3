1bb9c0fdc9273196653242501cd4f3d08914aa7a
unknown <mahmodulshahin665@gmail.com>
2022-03-21 17:17:15 +0600

    add btn handler

--- app.js
+++ app.js
@@ -16,5 +16,25 @@ document.getElementById('add-item').addEventListener('click', function () {
     const parent = document.getElementById('item-container');
     parent.appendChild(li);
 });
+//=================
+let add = 0;
+document.getElementById('add-number').addEventListener('click', function () {
+    add = add + 1;
+    if (add <= 5) {
+        document.getElementById('add-num-input').value = add;
+    }
+})
+document.getElementById('add-num-input').addEventListener('keyup', function () {
+    const addNumber = document.getElementById('add-number');
+    if (event.target.value == 'f') {
+        addNumber.removeAttribute('disabled')
+    }
+    else {
+        addNumber.setAttribute('disabled', true)
+    }
 
-
+})
+//chanage
+document.getElementById('add-num-input').addEventListener('change', function () {
+    const addField = document.getElementById('add-num-input');
+})
